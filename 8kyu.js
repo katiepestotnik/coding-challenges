e960300802860121f81b4915265265ecff9cdf45
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-08-23 20:51:54 -0600

    1 js 8kyu sooo tired today

--- 8kyu.js
+++ 8kyu.js
@@ -200,3 +200,24 @@ function countBy(x, n) {
 console.log(countBy(2, 5))
 //[2,4,6,8,10]
 // 1*2, 2*2, 3*2, 4*2, 5*2
+
+
+//Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+function countSheeps(arrayOfSheep) {
+    let count = 0
+    arrayOfSheep.forEach((sheep) => {
+        if (sheep === true) {
+            count += 1
+        } else {
+            count += 0
+        }
+    })
+    return count
+}
+  
+console.log(countSheeps([true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true]))
